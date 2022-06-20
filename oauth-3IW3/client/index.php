4844e6c4a22d743a88ca6d3dfb9e78f8498ef04b
Karl MARQUES <kmarques@vetixy.com>
2022-06-20 12:30:49 +0200

    3IW3: client password + Facebook oauth

--- oauth-3IW3/client/index.php
+++ oauth-3IW3/client/index.php
@@ -9,23 +9,46 @@ function login()
         "scope" => "read,write",
         "state" => bin2hex(random_bytes(16))
     ));
-    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Se connecter via Oauth Server</a>";
+    echo "
+        <form action='callback' method='POST'>
+            <input type='text' name='username'>
+            <input type='text' name='password'>
+            <input type='submit' value='Login'>
+        </form>
+    ";
+    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Se connecter via Oauth Server</a><br/>";
+    $queryParams= http_build_query(array(
+        "client_id" => "1010755216459252",
+        "redirect_uri" => "http://localhost:8081/fb_callback",
+        "response_type" => "code",
+        "scope" => "public_profile,email",
+        "state" => bin2hex(random_bytes(16))
+    ));
+    echo "<a href=\"https://www.facebook.com/v2.10/dialog/oauth?{$queryParams}\">Se connecter via Facebook</a>";
 }
 
 function callback()
 {
-    $code = $_GET['code'];
-    $state = $_GET['state'];
+    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        $specifParams = [
+            "grant_type" => "password",
+            "username" => $_POST["username"],
+            "password" => $_POST["password"]
+        ];
+    } else {
+        $specifParams = [
+            "grant_type" => "authorization_code",
+            "code" => $_GET["code"],
+        ];
+    }
     $clientId = "621e3b8d1f964";
     $clientSecret = "621e3b8d1f966";
     $redirectUri = "http://localhost:8081/callback";
-    $data = http_build_query(array(
-        "grant_type" => "authorization_code",
-        "code" => $code,
+    $data = http_build_query(array_merge([
         "redirect_uri" => $redirectUri,
         "client_id" => $clientId,
         "client_secret" => $clientSecret
-    ));
+    ], $specifParams));
     $url = "http://oauth-server:8080/token?{$data}";
     $result = file_get_contents($url);
     $result = json_decode($result, true);
@@ -44,6 +67,38 @@ function callback()
     echo "Hello {$result['lastname']}";
 }
 
+function fbcallback()
+{
+    $specifParams = [
+            "grant_type" => "authorization_code",
+            "code" => $_GET["code"],
+        ];
+    $clientId = "1010755216459252";
+    $clientSecret = "b0c27b63308d46ae5d236d2bd691921b";
+    $redirectUri = "http://localhost:8081/fb_callback";
+    $data = http_build_query(array_merge([
+        "redirect_uri" => $redirectUri,
+        "client_id" => $clientId,
+        "client_secret" => $clientSecret
+    ], $specifParams));
+    $url = "https://graph.facebook.com/v2.10/oauth/access_token?{$data}";
+    $result = file_get_contents($url);
+    $result = json_decode($result, true);
+    $accessToken = $result['access_token'];
+
+    $url = "https://graph.facebook.com/v2.10/me";
+    $options = array(
+        'http' => array(
+            'method' => 'GET',
+            'header' => 'Authorization: Bearer ' . $accessToken
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $result = json_decode($result, true);
+    echo "Hello {$result['name']}";
+}
+
 $route = $_SERVER['REQUEST_URI'];
 switch (strtok($route, "?")) {
     case '/login':
@@ -51,6 +106,9 @@ switch (strtok($route, "?")) {
         break;
     case '/callback':
         callback();
+        break;
+    case '/fb_callback':
+        fbcallback();
         break;
     default:
         echo '404';

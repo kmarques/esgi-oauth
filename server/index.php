41fa759af49f088a416d397218b951964d380c4e
Karl MARQUES <kmarques@vetixy.com>
2022-11-21 17:02:23 +0100

    OAUTH: authorizatio code

--- server/index.php
+++ server/index.php
@@ -1,12 +1,21 @@
 <?php
 
+function checkOrCreate(string $filename)
+{
+    if (!file_exists($filename)) {
+        touch($filename);
+    }
+}
+
 function readDatabase(string $filename): array
 {
+    checkOrCreate($filename);
     return array_map(fn ($item) => json_decode($item, true), file($filename));
 }
 
 function writeDatabase(string $filename, array $data): void
 {
+    checkOrCreate($filename);
     file_put_contents(
         $filename,
         implode("\n", array_map(fn ($item) => json_encode($item), $data))
@@ -42,10 +51,28 @@ function findApp(array $criteria): array|null
 {
     return findOneRow('./data/app.data', $criteria);
 }
+function findCode(array $criteria): array|null
+{
+    return findOneRow('./data/code.data', $criteria);
+}
+function findToken(array $criteria): array|null
+{
+    return findOneRow('./data/token.data', $criteria);
+}
 
 function insertApp(array $app): array
 {
     return insertRow('./data/app.data', $app);
+}
+
+function insertCode(array $app): array
+{
+    return insertRow('./data/code.data', $app);
+}
+
+function insertToken(array $app): array
+{
+    return insertRow('./data/token.data', $app);
 }
 
 
@@ -72,10 +99,113 @@ function register(): void
     echo json_encode($row);
 }
 
+function auth()
+{
+    ['client_id'=> $clientId, 'scope' => $scope, 'redirect_uri'=> $redirect, "state" => $state] = $_GET;
+    if (($app = findApp(['client_id'=> $clientId])) === null) {
+        return http_response_code(401);
+    }
+    if ($app['redirect_success'] !== $redirect) {
+        return http_response_code(401);
+    }
+
+    echo "<div>
+        <h1>Confirm</h1>
+        <p>{$app['name']} - {$app['url']}</p>
+        <p>{$scope}</p>
+        <a href=''>Cancel</a>
+        <a href='/auth-success?state={$state}&client_id={$clientId}'>Confirm</a>
+    </div>";
+}
+
+function authSuccess()
+{
+    ["state" => $state, "client_id"=> $clientId] = $_GET;
+    $app = findApp(['client_id'=> $clientId]);
+    $codeEntity = insertCode([
+        'code'=> sha1(uniqid()),
+        'user_id'=> 1,
+        'app_id' => $clientId,
+        'expiresAt'=> (new \DateTimeImmutable())->modify("+5 minutes")->getTimestamp()
+    ]);
+
+    header("Location: {$app['redirect_success']}?code={$codeEntity['code']}&state={$state}");
+}
+
+function token()
+{
+    ['client_id' => $clientId, 'code'=> $code, 'client_secret' => $secret, 'redirect_uri'=> $redirect] = $_GET;
+    if (($app = findApp(['client_id'=> $clientId, 'client_secret' => $secret, 'redirect_success' => $redirect])) === null) {
+        return http_response_code(401);
+    }
+    if (($codeEntity = findCode(['code'=> $code, 'app_id'=> $clientId]))=== null) {
+        return http_response_code(401);
+    }
+    if ((new \DateTimeImmutable())->setTimestamp($codeEntity['expiresAt']) < new \DateTimeImmutable()) {
+        return http_response_code(401);
+    }
+
+    $token = insertToken([
+        "token" => sha1(uniqid()),
+        "app_id" => $clientId,
+        "expiresIn"=> (new \DateTimeImmutable())->modify("+7 days")->getTimestamp(),
+        "user_id" => $codeEntity['user_id']
+    ]);
+
+    header('Content-Type: application/json');
+    echo json_encode([
+        'access_token' => $token['token'],
+        'expiresIn'=> $token['expiresIn']
+    ]);
+}
+
+function me()
+{
+    $headers = getallheaders();
+    $authorization = $headers['Authorization'] ?? $headers['authorization'];
+    if (!$authorization) {
+        return http_response_code(401);
+    }
+    if (!str_starts_with($authorization, 'Bearer')) {
+        return http_response_code(401);
+    }
+
+    $token = str_replace('Bearer ', '', $authorization);
+
+    $tokenEntity = findToken(['token'=> $token]);
+
+    if ((new \DateTimeImmutable())->setTimestamp($tokenEntity['expiresIn']) < new \DateTimeImmutable()) {
+        return http_response_code(401);
+    }
+
+    $userId = $tokenEntity['user_id'];
+
+    // get user in database
+
+    header('Content-Type: application/json');
+    echo json_encode([
+        'id' => $userId,
+        'name'=> 'Doe',
+        'firstname' => "John"
+    ]);
+}
+
 $url = strtok($_SERVER['REQUEST_URI'], '?');
 switch($url) {
     case '/register':
         register();
+        break;
+    case '/auth':
+        auth();
+        break;
+    case '/auth-success':
+        authSuccess();
+        break;
+    case '/token':
+        token();
+        break;
+    case '/me':
+        me();
         break;
     default:
         http_response_code(404);
